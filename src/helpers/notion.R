a8c3fb7632de286382c5ff1437467dce1f079329
Brandon Kramer <brandonleekramer@gmail.com>
2024-05-11 16:48:57 -0400

    Adding autoallocator script

---
+++ src/helpers/notion.R
@@ -0,0 +1,207 @@
+getNotionDatabase <- function(secret, database, filters = NULL, show_progress = FALSE, all_pages = TRUE, cover_icon = FALSE){
+  options(dplyr.summarise.inform = FALSE) # to supress all the grouping warnings!
+  
+  # +++++++++ construct headers
+  headers = c(`Authorization` = secret, `Notion-Version` = '2022-02-22', `Content-Type` = 'application/json' )
+  
+  # +++++++++ api call -------------------------------------------------------------
+  callAPI <- function(database, headers, filters, cursor){
+    res <- httr::POST(url = paste0('https://api.notion.com/v1/databases/', database, '/query'),
+                      httr::add_headers(.headers = headers),
+                      body = list("filter" = filters,
+                                  "start_cursor" = cursor),
+                      # start_cursor = cursor,
+                      encode = "json")
+    if(show_progress){ print(paste0("!! API Call: https://api.notion.com/v1/databases/", database, "/query")) }
+    
+    return( httr::content(res) )
+  }
+  
+  # +++++++++ this function "flattens" the results into a usable data.frame with 1 row per page (like the real database)
+  getItemsAndFlattenIntoDataFrame <- function(results, cover_and_icon = cover_icon){
+    if(show_progress){ print(paste0("- flattening into data.frame")) }
+    
+    # adding error-catching when the results are none (i.e., a filter w no results)
+    if(length(results) < 1 ){
+      dd <- data.frame("results" = "none")
+      warning("There are no results from the query. Check your filters. A data.frame will still be exported, with col_name = results and row1 = none")
+      
+    }else{
+      # the results (i.e., rows) are extracted into a simple data.frame with value being a list of each item's properties and id's
+      items <- tibble::enframe(results)
+      
+      # now, for each item, we will extract a tidy data.frame where we have all of the columns
+      dd <- NULL
+      for(i in 1:nrow(items)){
+        ## before we tidy up,
+        ## add NA's if there is no cover or icon AND we want to based on the option in the parameters of the function
+        
+        if(cover_and_icon){
+          if(is.null(  items[[2]][[i]][["cover"]] )){
+            items[[2]][[i]][["cover"]] <- as.logical("FALSE")
+          }
+          if(is.null( items[[2]][[i]][["icon"]] )){
+            items[[2]][[i]][["icon"]] <- as.logical("FALSE")
+          }
+        }
+        
+        # this is a tidy dataset with column 1 = name (i.e., value.object.type, etc) and col2 = value (i.e,. d3f0ee76-fc3b-426c-8d23-cff84800b0d6)
+        tmp <- tibble::enframe(unlist(items[[i, 2]]))
+        
+        # to avoid duplicates, (such as two relationships tied to a page) I will condense them into 1 separated by a pipe
+        tmp <- tmp %>%
+          group_by(name) %>%
+          summarise("value" = paste(value, collapse = " | "))
+        
+        # now, I want to keep this as 1 row in a big data set, so I will pivot_wider
+        tmp <- tidyr::pivot_wider(tmp)
+        
+        # now, I will create one big dataset, I will use dplyr in case columns are not exactly the same, which could be the case if one or various of the properties are missing
+        dd <- dplyr::bind_rows(dd, tmp)
+      }
+    }
+    
+    return(dd)
+  }
+  
+  if(all_pages){
+    if(show_progress){ print(paste0("++++ PAGINATING CALLS: ")) }
+    
+    # if using all_pages, I will run all of the pagination available.
+    new_cursor <- TRUE
+    dd <- NULL
+    cursor <- NULL
+    
+    while( new_cursor ){
+      if(show_progress){ print(paste0("- cursor: ", cursor, " / new_cursor: ", new_cursor )) }
+      
+      r <- callAPI(database = database,
+                   headers = headers,
+                   filters = filters,
+                   cursor = cursor)
+      
+      new_cursor <- r$has_more
+      cursor <- r$next_cursor
+      
+      # stack the data.frames together
+      tmp <- getItemsAndFlattenIntoDataFrame( r$results )
+      
+      dd <- dplyr::bind_rows(dd, tmp)
+      
+      if(show_progress){ print(paste0("- nrow of downloads: ", nrow(dd) )) }
+    }
+  }else{
+    # no pagination, just the top 100
+    if(show_progress){ print(paste0("++++ NO PAGINATION: ")) }
+    cursor <- NULL
+    r <- callAPI(database = database,
+                 headers = headers,
+                 filters = filters,
+                 cursor = cursor)
+    
+    dd <- getItemsAndFlattenIntoDataFrame( r$results )
+  }
+  
+  return(dd)
+}
+
+updateNumber <- function(secret, id, property_name, value){
+  
+  payload  <- sprintf("{\"properties\":{\"%s\":{\"number\":%s}}}",
+                      property_name, value
+  )
+  
+  auth_secret <- paste0("Bearer ", secret)
+  
+  headers = c(
+    `Authorization` = auth_secret,
+    `Notion-Version` = '2022-02-22',
+    `Content-Type` = 'application/json' )
+  
+  res <- httr::PATCH(url = paste0('https://api.notion.com/v1/pages/', id),
+                     httr::add_headers(.headers = headers),
+                     body = payload,
+                     encode = "json")
+  d <- httr::content(res)
+  return(d)
+}
+
+updateText <- function(secret, id, property_name, value){
+  
+  payload  <- sprintf(
+    "{\"properties\":{\"%s\":{\"rich_text\":[{\"type\":\"text\",\"text\":{\"content\": \"%s\"}}]}}}",
+    property_name, value
+  )
+  
+  auth_secret <- paste0("Bearer ", secret)
+  
+  headers = c(
+    `Authorization` = auth_secret,
+    `Notion-Version` = '2022-02-22',
+    `Content-Type` = 'application/json' )
+  
+  res <- httr::PATCH(url = paste0('https://api.notion.com/v1/pages/', id),
+                     httr::add_headers(.headers = headers),
+                     body = payload,
+                     encode = "json")
+  d <- httr::content(res)
+  return(d)
+}
+
+updateRelation <- function(secret, id, property_name, value){
+  
+  if(length(value) == 1){
+    payload  <- sprintf(
+      "{\"properties\":{\"%s\":{\"relation\":[{\"id\":\"%s\"}]}}}",
+      property_name, value
+    )
+  } else {
+    options <- paste0('{\"id\":\"',value,'\"}', collapse = ",")
+    payload  <- paste0(
+      '{\"properties\":{\"', property_name,'\":{\"relation\":[', options, ']}}}'
+    )
+  }
+  
+  auth_secret <- paste0("Bearer ", secret)
+  
+  headers = c(
+    `Authorization` = auth_secret,
+    `Notion-Version` = '2022-02-22',
+    `Content-Type` = 'application/json' )
+  
+  res <- httr::PATCH(url = paste0('https://api.notion.com/v1/pages/', id),
+                     httr::add_headers(.headers = headers),
+                     body = payload,
+                     encode = "json")
+  d <- httr::content(res)
+  return(d)
+}
+
+updateMultiSelect <- function(secret, id, property_name, value){
+  
+  if(length(value) == 1){
+    payload  <- sprintf(
+      "{\"properties\":{\"%s\":{\"multi_select\":[{\"name\":\"%s\"}]}}}",
+      property_name, value
+    )
+  }else{
+    options <- paste0('{\"name\":\"',value,'\"}', collapse = ",")
+    payload  <- paste0(
+      '{\"properties\":{\"', property_name,'\":{\"multi_select\":[', options, ']}}}'
+    )
+  }
+  
+  auth_secret <- paste0("Bearer ", secret)
+  
+  headers = c(
+    `Authorization` = auth_secret,
+    `Notion-Version` = '2022-02-22',
+    `Content-Type` = 'application/json' )
+  
+  res <- httr::PATCH(url = paste0('https://api.notion.com/v1/pages/', id),
+                     httr::add_headers(.headers = headers),
+                     body = payload,
+                     encode = "json")
+  d <- httr::content(res)
+  return(d)
+}
